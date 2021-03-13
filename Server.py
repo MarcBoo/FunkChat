c08e44a298a46b9ddaa50963d54b3fb404ffb7ee
Marc Bo <m.bonacker@freenet.de>
2021-03-13 16:22:59 +0100

    Authentifizierung funktioniert ohne delete
    Delete des Usernamens nach Disco funktioniert noch nicht, kb mehr gehabt

--- Server.py
+++ Server.py
@@ -17,20 +17,16 @@ def accept_incoming_connections():
 def handle_client(client):  # Takes client socket as argument.
     """Handles a single client connection."""
 
+    # Looks super trash but works
+    # Username saved in name
+    # if not already in it, will be appended to checkUsers-list
+    # delete after leaving does not work yet
     name = client.recv(BUFSIZ).decode("utf8")
-    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
-    clients[client] = name
-
-
-    """ name = client.recv(BUFSIZ).decode("utf8")
-
-    if name in clients:
+    if name in checkUsers:
         client.send(bytes("Your name is already taken, bye", "utf8"))
         client.close()
         del clients[client]
+        del checkUsers[name]
         broadcast(bytes("%s has left the chat." % name, "utf8"))
     else:
         welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
@@ -38,13 +34,13 @@ def handle_client(client):  # Takes client socket as argument.
         msg = "%s has joined the chat!" % name
         broadcast(bytes(msg, "utf8"))
         clients[client] = name
-
-        print(clients.items()) """
+        print(clients)
+        checkUsers.append(name)
 
     while True:
         msg = client.recv(BUFSIZ)
         if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name+": ")
+            broadcast(msg, name + ": ")
         else:
             client.send(bytes("{quit}", "utf8"))
             client.close()
@@ -57,9 +53,10 @@ def broadcast(msg, prefix=""):  # prefix is for name identification.
     """Broadcasts a message to all the clients."""
 
     for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
+        sock.send(bytes(prefix, "utf8") + msg)
 
-        
+
+checkUsers = []
 clients = {}
 addresses = {}
 
@@ -78,10 +75,3 @@ if __name__ == "__main__":
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
     SERVER.close()
-
-
-
-
-
-
-
