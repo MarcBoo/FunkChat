5fa1423b81c155321f07446a5151ba59627bac78
Marc Bo <m.bonacker@freenet.de>
2021-03-16 12:12:52 +0100

    name convention server.py

--- Server.py
+++ Server.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
-"""Server for multithreaded (asynchronous) chat application."""
+"""Server Application for our Multithreaded and Asynchronous Chat Application"""
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
 
-def accept_incoming_connections():
-    """Sets up handling for incoming clients."""
+def client_new_connection():
+    """Controls connections of new clients"""
     while True:
         client, client_address = SERVER.accept()
         print("%s:%s has connected." % client_address)
         client.send(bytes("Now type your name and press enter!", "utf8"))
         addresses[client] = client_address
-        Thread(target=handle_client, args=(client,)).start()
+        Thread(target=client_control, args=(client,)).start()
 
 
-def handle_client(client):  # Takes client socket as argument.
-    """Handles a single client connection."""
+def client_control(client):  # Takes client socket as argument.
+    """Controls interaction with a single client."""
 
 
     # Username saved in name
@@ -98,7 +98,7 @@ SERVER.bind(ADDR)
 if __name__ == "__main__":
     SERVER.listen(5)
     print("Waiting for connection...")
-    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD = Thread(target=client_new_connection)
     ACCEPT_THREAD.start()
     ACCEPT_THREAD.join()
     SERVER.close()
